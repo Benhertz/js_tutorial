ee8f81ba343a1c488e5ca6219682846d204c8728
Ben Hertzberg <benhertz@gmail.com>
2018-05-16 23:49:56 -0700

    tooling around

--- hello.js
+++ hello.js
@@ -1,3 +1,6 @@
     console.log("hello, world!");
-    console.log("hello, world!", "how's\nit going?");
-    
+    console.log("hello, world!", "how's it going?");
+    // prints hello world
+    let stringA = "motherFucker";
+    let long = stringA.length;
+    console.log(long);
