f2919c4f0ac54b8697fe1b4fc0a790d9ea4ad649
Ben Hertzberg <benhertz@gmail.com>
2018-05-24 03:30:02 +0000

    javascript change

--- hello.js
+++ hello.js
@@ -1,6 +1,10 @@
-    console.log("hello, world!");
-    console.log("hello, world!", "how's it going?");
-    // prints hello world
-    let stringA = "motherFucker";
-    let long = stringA.length;
-    console.log(long);
+    // console.log("hello, world!");
+    // console.log("hello, world!", "how'sit going ? ");
+    // // prints hello world
+    // let stringA = "motherFucker";
+    // let long = stringA.length;
+    // console.log(long);
+    let reg = /\w+/g;
+    let strings = "eat More every day and night until you blow up into a ball of bloody fat type";
+    console.log(strings.match(reg));
+    
