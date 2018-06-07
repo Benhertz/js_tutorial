240ae8829e5468cc68fec16c99a7a48dfaca82f5
Ben Hertzberg <benhertz@gmail.com>
2018-06-06 21:55:06 -0700

    add arrow function version

--- functional.js
+++ functional.js
@@ -1,23 +1,29 @@
 let states = ["Kansas", "Nebraska", "North Dakota", "South Dakota"];
 
 function imperativeMap(states) {
-    let urlStates = [];
-    states.forEach(function(state) {
-        urlStates.push(state.toLowerCase().split(/\s+/).join("-"));
-    });
-    return urlStates;
+  let urlStates = [];
+  states.forEach(function(state) {
+    urlStates.push(state.toLowerCase().split(/\s+/).join("-"));
+  });
+  return urlStates;
 }
 console.log(imperativeMap(states));
 
 
 let a = ["fun", "sun", 66.666, "little", "bitch"];
 
-let func = function(a) {
-    console.log(a);
+let func = (states) => {
+  let urlStates = [];
+  states.forEach(state => {
+    urlStates.push(state.toLowerCase().split(/\s+/).join("-"));
+  });
+  return urlStates;
 };
 
+console.log(func(states));
+
 a.forEach(function(x) {
-    console.log(x);
+  console.log(x);
 });
 
 let str = 'string between';
@@ -27,7 +33,7 @@ console.log(array);
 console.log(array.join(""));
 
 let reverse = string => {
-    return string.split("").reverse().join("");
+  return string.split("").reverse().join("");
 
 }
 console.log(reverse("have fun"));
